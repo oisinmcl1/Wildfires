349396840dac168e7c483fc9fc900b2a47574756
oisinmcl1 <omclaughlin99@gmail.com>
2025-10-10 11:29:01 +0100

    reading training and test data, added pandas to requirements.txt

--- extension.py
+++ extension.py
@@ -1,5 +1,13 @@
 """
-Python script that prints "hello world"
+PLACEHOLDER
 """
 
-print("hello world")
+import pandas as pd
+
+# Read the training and test data csv file with pandas
+train_data = pd.read_csv('wildfires_training.csv')
+test_data = pd.read_csv('wildfires_test.csv')
+
+print("Reading in the data:")
+print("Training data shape:", train_data.shape)
+print("Test data shape:", test_data.shape)
