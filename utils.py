186f3612154f3a1c8d759dabd925f45ab82e815e
oisinmcl1 <omclaughlin99@gmail.com>
2025-10-14 19:25:58 +0100

    util function to stop code dupplication

---
+++ utils.py
@@ -0,0 +1,37 @@
+"""
+UTIL SCRIPT
+Oisin Mc Laughlin
+22441106
+"""
+
+import pandas as pd
+
+def get_data():
+    """
+    Utility function to read in the wildfire training and test data from CSV files,
+    separate features and target variable and return them for model training and evaluation.
+
+    Returns:
+        x_train, y_train, x_test, y_test
+    """
+    # Read the training and test data csv file with pandas
+    train_data = pd.read_csv('wildfires_training.csv')
+    test_data = pd.read_csv('wildfires_test.csv')
+
+    print("\nTraining Data:")
+    print(train_data.info())
+
+    print("\nTest Data:")
+    print(test_data.info())
+
+    # Separate features and target data
+    x_train = train_data.drop('fire', axis=1)
+    y_train = train_data['fire']
+    x_test = test_data.drop('fire', axis=1)
+    y_test = test_data['fire']
+
+    print("\nFeatures and targets seperated:")
+    print(x_train.head())
+    print(y_train.head())
+
+    return x_train, y_train, x_test, y_test
